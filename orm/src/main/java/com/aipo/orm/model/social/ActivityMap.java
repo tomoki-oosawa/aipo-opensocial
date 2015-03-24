f5a545a9223ef327ed0b71cc5bad90d5d6232c31
nbeppu <nbeppu@gmail.com>
2015-03-24 17:34:47 +0900

    Normalize all the line endings

--- orm/src/main/java/com/aipo/orm/model/social/ActivityMap.java
+++ orm/src/main/java/com/aipo/orm/model/social/ActivityMap.java
@@ -1,6 +1,6 @@
 /*
  * Aipo is a groupware program developed by Aimluck,Inc.
- * Copyright (C) 2004-2011 Aimluck,Inc.
+ * Copyright (C) 2004-2015 Aimluck,Inc.
  * http://www.aipo.com
  *
  * This program is free software: you can redistribute it and/or modify
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package com.aipo.orm.model.social;
 
 import com.aipo.orm.model.social.auto._ActivityMap;
