b3e6bc056384c1ee976e0048100a9a1087029fae
you12724 <id-12724.s.v@ruri.waseda.jp>
2016-03-15 18:33:11 +0900

    refs #36898 テスト用に5にしていたのを300に変更

--- social-api/src/main/java/com/aipo/social/opensocial/service/AipoMessageRoomHandler.java
+++ social-api/src/main/java/com/aipo/social/opensocial/service/AipoMessageRoomHandler.java
@@ -115,7 +115,7 @@ public class AipoMessageRoomHandler {
       AipoPreconditions.required("member_to", memberList);
       AipoPreconditions.multiple("member_to", memberList);
       AipoPreconditions.maxLength("name", name, 50);
-      AipoPreconditions.maxMember("member", memberList.size(), 5);
+      AipoPreconditions.maxMember("member", memberList.size(), 300);
       AipoPreconditions.isUTF8("name", name);
 
       return service.postRoom(
